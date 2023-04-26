a5716ccad8b90dbc1f0cbb6b88255dad24adb12b
Robson Silva <robsantossilva@gmail.com>
2023-04-25 22:04:32 -0300

    creating subnets

--- cluster-k8s-aws/vpc.tf
+++ cluster-k8s-aws/vpc.tf
@@ -4,3 +4,26 @@ resource "aws_vpc" "new-vpc" {
     Name = "fullcycle-vpc"
   }
 }
+
+# data "aws_availability_zones" "available" {}
+# output "az" {
+#   value = data.aws_availability_zones.available.names
+# }
+
+resource "aws_subnet" "new-subnet-1" {
+  availability_zone = "us-east-1a"
+  vpc_id            = aws_vpc.new-vpc.id
+  cidr_block        = "10.0.0.0/24"
+  tags = {
+    Name = "fullcycle-subnet-1"
+  }
+}
+
+resource "aws_subnet" "new-subnet-2" {
+  availability_zone = "us-east-1b"
+  vpc_id            = aws_vpc.new-vpc.id
+  cidr_block        = "10.0.1.0/24"
+  tags = {
+    Name = "fullcycle-subnet-2"
+  }
+}
