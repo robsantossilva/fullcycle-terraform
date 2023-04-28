7b783028ab1decb80d90eb6ec983e4fb0002c138
Robson Silva <robsantossilva@gmail.com>
2023-04-28 16:31:26 -0300

    internet gateway, route table and route table associated

--- cluster-k8s-aws/2-vpc.tf
+++ cluster-k8s-aws/2-vpc.tf
@@ -38,3 +38,27 @@ resource "aws_subnet" "subnets" {
 #     Name = "${var.prefix}-subnet-2"
 #   }
 # }
+
+resource "aws_internet_gateway" "new-igw" {
+  vpc_id = aws_vpc.new-vpc.id
+  tags = {
+    Name = "${var.prefix}-igw"
+  }
+}
+
+resource "aws_route_table" "new-rtb" {
+  vpc_id = aws_vpc.new-vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.new-igw.id
+  }
+  tags = {
+    Name = "${var.prefix}-rtb"
+  }
+}
+
+resource "aws_route_table_association" "new-rtb-association" {
+  count          = 2
+  route_table_id = aws_route_table.new-rtb.id
+  subnet_id      = aws_subnet.subnets.*.id[count.index]
+}
